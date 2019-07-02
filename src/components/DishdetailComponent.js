36886b6362c7e7f7f559cc9debcba68cb0512de6
Jyothi Kiran <r.jyothikiranreddy@gmail.com>
2019-07-02 12:23:36 +0530

    SPA 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Card, CardImg,CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-
-
-
-   function RenderDish({dish}) {
+    function RenderDish({dish}) {
         return (
-            <div className="col-12 col-md-5 m-1">
             <Card>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardBody>
@@ -14,7 +11,6 @@ import { Card, CardImg,CardText, CardBody, CardTitle } from 'reactstrap';
                     <CardText>{dish.description}</CardText>
                 </CardBody>
             </Card>
-            </div>
         );
     }
 
@@ -24,7 +20,7 @@ import { Card, CardImg,CardText, CardBody, CardTitle } from 'reactstrap';
                 <li key={comment.id} >
                     {comment.comment}
                     <br /><br />
-                    -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                    -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
                     <br /><br />
                 </li>
             );
@@ -39,18 +35,29 @@ import { Card, CardImg,CardText, CardBody, CardTitle } from 'reactstrap';
             </div>
         );
     }
-    const DishDetail = (props) =>{
+
+    const DishDetail = props => {
         if (props.dish) {
             return (
                 <div className="container">
-                <div className="row">
-                    
-                <RenderDish dish={props.dish}/>
-            
-            <div className="col-12 col-md-5 m-1">
-                <RenderComments comments={props.dish.comments}/>
-            </div>
-                </div>
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderDish dish={props.dish} />
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderComments comments={props.comments} />
+                        </div>
+                    </div>
                 </div>
             );
         }
@@ -60,7 +67,5 @@ import { Card, CardImg,CardText, CardBody, CardTitle } from 'reactstrap';
             );
         }
     }
-
-    
 
 export default DishDetail;
